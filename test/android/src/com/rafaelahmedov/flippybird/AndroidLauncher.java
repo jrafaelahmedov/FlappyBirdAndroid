ad998c8764d62e98a3a544d6cc302394c69c5ad0
rafael <rafaelahmedovjava@gmail.com>
2018-12-11 17:11:09 +0400

    flatbird

---
+++ test/android/src/com/rafaelahmedov/flippybird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.rafaelahmedov.flippybird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.rafaelahmedov.flippybird.FlippyBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlippyBird(), config);
+	}
+}
